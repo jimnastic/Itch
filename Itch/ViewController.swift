8cee8898ba2021d7c8fc440e5264cb4d0bd9ce05
Jim May <jim_nastic@hotmail.com>
2016-11-19 07:26:29 +1100

    Initial Commit

---
+++ Itch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Itch
+//
+//  Created by James May on 19/11/16.
+//  Copyright © 2016 James May. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
